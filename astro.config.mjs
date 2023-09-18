dd2cf38241583e24883cd99ab9735503bbc5084c
Stefano Faieta <9802152+stefanofa@users.noreply.github.com>
2023-09-18 23:45:18 +0200

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://stefanofa.it",
+  integrations: [tailwind(), sitemap()],
   output: "static",
   adapter: vercel({
     webAnalytics: {
